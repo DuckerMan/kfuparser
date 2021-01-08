9acdac89c1cdf267c6fc4a8c6b526ec358528aee
tganiullin <tganiullin@yandex.ru>
2021-01-08 17:35:45 +0300

    Тестовое изменение

--- Parser.class.php
+++ Parser.class.php
@@ -1,1 +1,13 @@
+<?php
 
+class Parser{
+    public function __construct(){
+
+    }
+
+    public function sendRequest($pageUrl, $token = NULL, $post = NULL){
+        $base_url = 'yandex.ru';
+        $pause_time = 2;
+        //$retry = 0;
+    }
+}
